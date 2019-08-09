b60c331f280f16b255c5fe6d572f6cb34361afb6
Belinda Lei <belinda.lei95@gmail.com>
2019-08-09 05:08:24 +0000

    Automatically backed up by Learn

--- lib/Model1.rb
+++ lib/Model1.rb
@@ -1,5 +1,32 @@
 #class for Model1 goes here
 #Feel free to change the name of the class
-class Model1
+class Doctor 
+
+  attr_reader :name, :school
+
+  @@all = []
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @degree = attributes[:degree]
+    @gender = "male"
+    @@all << self 
+    # puts @name
+    # puts @degree
+  end 
+
+  def self.all
+    @@all
+  end
+
+
+  #see all the APPTs I have for today 
+
+  def my_appointments 
+    #look through all the appointments 
+    my_appointments == Appointment.all.select do |apt|
+      apt.doctor == self 
+    end 
+  end 
 
 end
